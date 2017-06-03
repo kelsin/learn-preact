27ffbd142f35abea4d8d1d352680b20a1d0bd500
Caitlin <ayashi@valefor.com>
2017-06-03 14:47:29 -0700

    combining reducers and removing pushes

---
+++ src/lib/reducers/chatroom.js
@@ -0,0 +1,57 @@
+import {
+	ADD_CHAT_LINE,
+	DELETE_CHAT_LINE,
+	ADD_USER_TO_CHATROOM,
+	DELETE_USER_FROM_CHATROOM
+} from "actions/types";
+
+let chatroom = function(chatroom = {}, action) {
+	switch (action.type) {
+		case ADD_CHAT_LINE:
+			if (chatroom.id === action.chatroomId) {
+				return {
+					...chatroom,
+					chat: [
+						...chatroom.chat,
+						{
+							sender: action.sender,
+							timestamp: action.timestamp,
+							body: action.body,
+							id: action.id
+						}
+					]
+				};
+			} else {
+				return chatroom;
+			}
+		case DELETE_CHAT_LINE:
+			if (chatroom.id === action.chatroomId) {
+				let newChatroom = Object.assign({}, chatroom);
+				newChatroom.chat = newChatroom.chat.filter(
+					({ id }) => id !== action.id
+				);
+				return newChatroom;
+			}
+			return chatroom;
+		case ADD_USER_TO_CHATROOM:
+			if (chatroom.id === action.chatroomId) {
+				return { ...chatroom, userIds: [...chatroom.userIds, action.userId] };
+			}
+			return chatroom;
+		case DELETE_USER_FROM_CHATROOM:
+			if (chatroom.id === action.chatroomId) {
+				let newChatroom = Object.assign({}, chatroom);
+
+				newChatroom.userIds = newChatroom.userIds.filter(
+					({ userId }) => userId !== action.userId
+				);
+				return newChatroom;
+			} else {
+				return chatroom;
+			}
+		default:
+			return chatroom;
+	}
+};
+
+export default chatroom;
---
+++ src/lib/reducers/chatrooms.js
@@ -0,0 +1,35 @@
+import {
+	ADD_CHAT_LINE,
+	DELETE_CHAT_LINE,
+	ADD_CHATROOM,
+	DELETE_CHATROOM,
+	ADD_USER_TO_CHATROOM,
+	DELETE_USER_FROM_CHATROOM
+} from "actions/types";
+
+import chatroom from './chatroom';
+
+let chatrooms = function(chatrooms = [], action) {
+	switch (action.type) {
+		case ADD_CHAT_LINE:
+		case DELETE_CHAT_LINE:
+		case ADD_USER_TO_CHATROOM:
+		case DELETE_USER_FROM_CHATROOM:
+			return chatrooms.map(function(room){
+				return chatroom(room, action);
+			});
+		case ADD_CHATROOM:
+			return [...chatrooms, {
+				name: action.name,
+				id: action.id,
+				chat: [],
+				userIds: []
+			}];
+		case DELETE_CHATROOM:
+			return chatrooms.filter(({ id }) => id !== action.chatroomId);
+		default:
+			return chatrooms;
+	}
+};
+
+export default chatrooms;

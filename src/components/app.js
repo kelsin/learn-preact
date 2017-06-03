c4991fe6f0a632d45dda9b531d212372820d11b6
Caitlin <ayashi@valefor.com>
2017-06-03 12:13:31 -0700

    adding redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,19 @@
+import { Provider, connect } from 'preact-redux';
 import { h, Component } from 'preact';
+import { createStore } from 'redux';
 import { Router } from 'preact-router';
+
+import reducers from '../lib/reducers';
+import initialState from '../lib/initState';
+import { addChatLine } from '../lib/actions/chat';
 
 import Header from './header';
 import Home from './home';
 import Profile from './profile';
+
+let store = createStore(reducers, initialState);
+
+store.dispatch(addChatLine(1, Math.floor(Date.now() / 1000), 'Hello everyone!'));
 
 export default class App extends Component {
 	/** Gets fired when the route changes.
@@ -17,12 +27,15 @@ export default class App extends Component {
 	render() {
 		return (
 			<div id="app">
+			<Provider store={store}>
 				<Header />
 				<Router onChange={this.handleRoute}>
 					<Home path="/" />
 					<Profile path="/profile/" user="me" />
 					<Profile path="/profile/:user" />
 				</Router>
+			</Provider>
+			});
 			</div>
 		);
 	}
---
+++ src/lib/actions/chat.js
@@ -0,0 +1,12 @@
+import { ADD_CHAT_LINE } from './types';
+
+function addChatLine(sender, timestamp, body) {
+	return {
+		type: ADD_CHAT_LINE,
+		sender,
+		timestamp,
+		body
+	}
+}
+
+export { addChatLine };
